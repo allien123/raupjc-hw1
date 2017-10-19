51f5fa080a0c37d6dbe3acb6ecff09095a15fac0
Alen Magdić <alen.magdic@fer.hr>
2017-10-19 16:57:08 +0200

    Klasa za iteraciju liste premještena unutar klase GenericList

--- ConsoleApp1/ConsoleApp1/IGenericList.cs
+++ ConsoleApp1/ConsoleApp1/IGenericList.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    public interface IGenericList<X>
+    public interface IGenericList<X>: IEnumerable<X>
     {
         void Add(X item);
         bool Remove(X item);
